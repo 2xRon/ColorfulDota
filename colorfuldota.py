53d25bf596e375c7f10f6dfca84cea968ad0c3b5
Byron Smith <smith.byronx@gmail.com>
2016-02-16 14:56:19 -0500

    added default color hex code

--- colorfuldota.py
+++ colorfuldota.py
@@ -2,7 +2,7 @@
 import sys, re
 
 def main():
-	colordict = {"olive":"\x10","pink":"\x11","red":"\x12","orange":"\x13","darkyellow":"\x14","lightgreen":"\x15","purple":"\x16","grey":"\x17","green":"\x18","blue":"\x19","white":"\x0B","limegreen":"\x0C","hotpink":"\x0E","violet":"\x1A","magenta":"\x1C","yellow":"\x1F"}
+	colordict = {"olive":"\x10","pink":"\x11","red":"\x12","orange":"\x13","darkyellow":"\x14","lightgreen":"\x15","purple":"\x16","grey":"\x17","green":"\x18","blue":"\x19","white":"\x0B","limegreen":"\x0C","hotpink":"\x0E","violet":"\x1A","magenta":"\x1C","yellow":"\x1F","default":"\x06"}
 	if len(sys.argv) != 2:
 		print("Call like 'python ./dotacolor.py \"YOUR STRING[[colorname]]STRING IN COLOR\"' Include the quotes. Copy the output into the game (on Windows, select and right-click)")
 		print("Color names:")
