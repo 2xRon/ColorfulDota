aa496c812aa1f75fa53961c6b9c465250778b82e
2xRon <smith.byronx@gmail.com>
2019-05-29 14:31:31 -0400

    updated arguemnt parsing. added clipboard support

--- colorfuldota.py
+++ colorfuldota.py
@@ -1,18 +1,82 @@
-# Colored dota texter
-import sys, re
+"""Colorful Dota. Replaces markers with special codes that correspond to certain text colors in Dota 2's chatbox."""
+
+import sys, re, argparse
+
+try:
+    import pyperclip
+except ImportError:
+    HASCLIP = False
+else:
+    HASCLIP = True
+
+colordict = {
+    "olive": "\x10",
+    "pink": "\x11",
+    "red": "\x12",
+    "orange": "\x13",
+    "darkyellow": "\x14",
+    "lightgreen": "\x15",
+    "purple": "\x16",
+    "grey": "\x17",
+    "green": "\x18",
+    "blue": "\x19",
+    "white": "\x0B",
+    "limegreen": "\x0C",
+    "hotpink": "\x0E",
+    "violet": "\x1A",
+    "magenta": "\x1C",
+    "yellow": "\x1F",
+    "default": "\x06",
+}
+description = __doc__ + "\nAvalible colors: " + ", ".join(colordict.keys())
+
+delimiter_match = {
+    "(": ("((", "))"),
+    ")": ("((", "))"),
+    "[": ("[[", "]]"),
+    "]": ("[[", "]]"),
+    "{": ("{{", "}}"),
+}
+
 
 def main():
-	colordict = {"olive":"\x10","pink":"\x11","red":"\x12","orange":"\x13","darkyellow":"\x14","lightgreen":"\x15","purple":"\x16","grey":"\x17","green":"\x18","blue":"\x19","white":"\x0B","limegreen":"\x0C","hotpink":"\x0E","violet":"\x1A","magenta":"\x1C","yellow":"\x1F","default":"\x06"}
-	if len(sys.argv) != 2:
-		print("Call like 'python ./dotacolor.py \"YOUR STRING[[colorname]]STRING IN COLOR\"' Include the quotes. Copy the output into the game (on Windows, select and right-click)")
-		print("Color names:")
-		for key, value in colordict.items():
-			print(key)
-		return -1
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument(
+        "-d",
+        "--delimiter",
+        type=str,
+        help="Set the delimiter for color names. Uses a matched delimiter for (, [ or {. Delimiters should be used like '[[color]]' or '//color//'.",
+    )
+    parser.add_argument(
+        "-c",
+        "--copy",
+        action="store_true",
+        help="Copy output to system clipboard. Requires pyperclip.",
+    )
+    parser.add_argument(
+        "-t",
+        "--entrytext",
+        type=str,
+        nargs=argparse.REMAINDER,
+        required=True,
+        help="Message to process",
+    )
+    args = parser.parse_args()
+    entrytext = " ".join(args.entrytext)
 
-	entrytext = sys.argv[1]
-	outtext = re.sub('\[\[(.*?)]\]',lambda x: colordict[x.group(0)[2:-2]], entrytext, flags=re.DOTALL)
-	print(outtext)
+    if args.delimiter:
+        # for delimiters without a matching pair, use the same symbol twice
+        default_d = (args.delimiter + args.delimiter, args.delimiter + args.delimiter)
+        d = delimiter_match.get(args.delimiter, default_d)
+    else:
+        d = delimiter_match["("]
+
+    pattern = re.compile(rf"{re.escape(d[0])}(.*?){re.escape(d[1])}", flags=re.DOTALL)
+    outtext = pattern.sub(lambda x: colordict.get(x.group(0)[2:-2], None), entrytext)
+    print(outtext)
+    if HASCLIP and args.copy:
+        pyperclip.copy(outtext)
+
 
 if __name__ == "__main__":
-	main()
+    main()
